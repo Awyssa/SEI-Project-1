b17923e708b91053565272ba5fdc5e128e04067b
Awyssa <michael.ehson@gmail.com>
2021-02-19 17:53:56 +0000

    toggle grid image test

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,41 @@
 function init() {
 
+  const images = ['rum', 'murray', 'duffman', 'theDude', 'corbyn', 'boris']
+
   // * Variables
   const grid = document.querySelector('.grid')
+
+  console.log(grid)
   
   const width = 10
   const cellCount = width * width
   const cells = []
 
-  // * Make a grid
   function createGrid() {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
       grid.appendChild(cell)
       cells.push(cell)
+      cell.id = i
+    }
+  }  
+  createGrid()
+
+  const addRandomImage = (event) => {
+    console.log(event.target)
+    event.target.classList.toggle(randomImage(images))
   }
-}
-  createGrid() 
- 
+
+  const randomImage = (array) => {
+    image = array[Math.floor(Math.random() * array.length)]
+    return image
+  }
+
+  grid.addEventListener('click', addRandomImage)
+
+
+
+  
 }
 
 
