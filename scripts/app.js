046cc673644d7a603ac7d038fd8658acfc58c527
Awyssa <michael.ehson@gmail.com>
2021-02-19 19:56:40 +0000

    random mines assinged working

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,14 @@
 function init() {
 
-  const images = ['rum', 'murray', 'duffman', 'theDude', 'corbyn', 'boris']
+
 
   // * Variables
   const grid = document.querySelector('.grid')
+  const gridCells = document.querySelectorAll('.grid')
 
-  console.log(grid)
+  console.log('gridcells', gridCells)
+
+  console.log('grid', grid)
   
   const width = 10
   const cellCount = width * width
@@ -17,28 +20,50 @@ function init() {
       grid.appendChild(cell)
       cells.push(cell)
       cell.id = i
+      // make an array for the mines? 
     }
   }  
   createGrid()
 
-  const addRandomImage = (event) => {
-    console.log(event.target)
-    event.target.classList.toggle(randomImage(images))
+  // Addmine function.
+  // The width is going to equal the amount of mines in the game 
+  // the function will take the width, and add that amount of mines 
+  // Using a for loop, add width amount of mines randomly among the cells 
+  // as the i ===  width, loop that many times adding mines to the grid.
+  // !!! NOT FOR NOW!!! Just leave it empty! LATER = for every other cell add a beer
+  //
+
+
+  function addMines(grid) {
+    for (i = 0; i < width; i++) {
+      let cellToAddMine =  grid[Math.floor(Math.random() * grid.length)]
+      console.log(cellToAddMine)
+      cellToAddMine.classList.add('mine')
+    }
   }
-
-  const randomImage = (array) => {
-    image = array[Math.floor(Math.random() * array.length)]
-    return image
-  }
-
-  grid.addEventListener('click', addRandomImage)
-
-
-
   
+  addMines(cells)
 }
 
 
-
+// for each grid item, add 10 random mines. Once complete ELSE add beer
 
 window.addEventListener('DOMContentLoaded', init)
+
+
+
+
+
+// redundant code 
+
+// const addRandomImage = (event) => {
+//   console.log(event.target)
+//   event.target.classList.toggle(randomImage(images))
+// }
+
+// const randomImage = (array) => {
+//   image = array[Math.floor(Math.random() * array.length)]
+//   return image
+// }
+
+// grid.addEventListener('click', addRandomImage)
