ba094cd908af70d9e9abe6ac317d7c7e47edaabb
Awyssa <michael.ehson@gmail.com>
2021-02-19 13:42:48 +0000

    starting

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# SEI-Project-1
+~~Minesweeper~~ COVID-SWEEPER!!!
+
+Its Minesweeper, but you have to avoid COVID-19 and collect beers instead! 
+
---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+function init() {
+
+  console.log('hello there!')
+
+
+
+
+
+
+
+
+}
+
+window.addEventListener('DOMContentLoaded', init)
+
+console.log('did this work?')
